089b8b004cf3c7766cf484cf8f843a6a1d873f62
Anatoly <code4webdev@gmail.com>
2026-06-13 10:00:21 +0500

    feat: добавить состояние для управления списком задач

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <h1>Todo App</h1>
