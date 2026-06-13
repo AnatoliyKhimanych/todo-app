c6d85e0980511506662f5bc871f32689367ff976
Anatoly <code4webdev@gmail.com>
2026-06-13 09:23:52 +0500

    feat: добавить заготовку для компонентов Todo, TodoForm и TodoList

---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,5 @@
+const Todo = () => {
+  return <h1>This is todo</h1>;
+};
+
+export default Todo;
---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,10 @@
+const TodoForm = () => {
+  return (
+    <form>
+      <input placeholder="Enter new todo" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default TodoForm;
