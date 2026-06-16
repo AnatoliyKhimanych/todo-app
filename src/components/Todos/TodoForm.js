7e6230c612889639f41297038c74c9b7d102e636
Anatoly <code4webdev@gmail.com>
2026-06-16 14:16:53 +0500

    feat: передать функцию добавления задачи в компонент TodoForm и реализовать обработку отправки формы

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,7 +1,21 @@
-const TodoForm = () => {
+import { useState } from 'react';
+
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState('');
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    addTodo(text); //? Добавляет текст в массив задач в компоненте App
+    setText(''); //? Чтобы обнулить состояние компонента TodoForm
+  };
+
   return (
-    <form>
-      <input placeholder="Enter new todo" />
+    <form onSubmit={onSubmitHandler}>
+      <input
+        placeholder="Enter some todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <button type="submit">Submit</button>
     </form>
   );
