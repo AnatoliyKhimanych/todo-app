080ce5ba32d4822c1e145256ecc448ba43ae95fa
Anatoly <code4webdev@gmail.com>
2026-08-12 16:16:53 +0500

    feat: добавить сообщение о пустом списке задач в компоненте TodoList

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -5,7 +5,7 @@ import styles from './TodoList.module.css'
 const TodoList = ({ todos }) => {
   return (
     <div className={styles.todoListContainer}>
-      {todos.map((todo, index) => <Todo key={index} todo={todo} />)}
+      {todos.length === 0 ? <h2>Todo list is empty</h2> : todos.map((todo, index) => <Todo key={index} todo={todo} />)}
     </div>
   ) 
 };
