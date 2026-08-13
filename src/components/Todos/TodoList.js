096b14713483c9fcc850368b6c1c686221083694
Anatoly <code4webdev@gmail.com>
2026-08-13 16:34:41 +0500

    feat: добавить возможность удаления задач из списка при двойном клике

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,13 +1,19 @@
 import Todo from './Todo';
 
-import styles from './TodoList.module.css'
+import styles from './TodoList.module.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo }) => {
   return (
     <div className={styles.todoListContainer}>
-      {todos.length === 0 ? <h2>Todo list is empty</h2> : todos.map((todo, index) => <Todo key={index} todo={todo} />)}
+      {todos.length === 0 ? (
+        <h2>Todo list is empty</h2>
+      ) : (
+        todos.map((todo, index) => (
+          <Todo key={index} todo={todo} index={index} deleteTodo={deleteTodo} />
+        ))
+      )}
     </div>
-  ) 
+  );
 };
 
 export default TodoList;
