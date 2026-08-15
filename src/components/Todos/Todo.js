9edf5b03413073c1e49e943eae671c2f9fabef73
Anatoly <code4webdev@gmail.com>
2026-08-15 23:36:58 +0500

    feat: добавить возможность удаления задачи по клику на компонент RiDeleteBin2Line

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -5,10 +5,10 @@ import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri';
 
 const Todo = ({ todo, deleteTodo }) => {
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
+    <div className={styles.todo}>
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
-      <RiDeleteBin2Line className={styles.deleteIcon} />
+      <RiDeleteBin2Line className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)}/>
       <FaCheck className={styles.checkIcon} />
     </div>
   );
