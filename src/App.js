c04fcf0b16568d8c59c39c628139ffede1d5f436
Anatoly <code4webdev@gmail.com>
2026-08-31 16:46:32 +0500

    feat: добавить возможность переключения состояния задачи

--- src/App.js
+++ src/App.js
@@ -19,11 +19,26 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const toggleTodoHandler = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo;
+      }),
+    );
+    console.log(todos);
+  };
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
-      <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodoHandler}
+        toggleTodo={toggleTodoHandler}
+      />
     </div>
   );
 }
--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -3,13 +3,13 @@ import { FaCheck } from 'react-icons/fa6';
 import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri';
 
 
-const Todo = ({ todo, deleteTodo }) => {
+const Todo = ({ todo, deleteTodo, toggleTodo }) => {
   return (
     <div className={styles.todo}>
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
       <RiDeleteBin2Line className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)}/>
-      <FaCheck className={styles.checkIcon} />
+      <FaCheck className={styles.checkIcon} onClick={() => toggleTodo(todo.id)} />
     </div>
   );
 };
